6f2e7404fc3ac413ce1931850cc206d28be4cf9c
Szabolcs Vereb <szabolcs.vereb@greenfoxacademy.com>
2017-05-16 17:14:31 +0200

    asdasd

---
+++ week-03/day-2/Atmel/Projekt/Projekt/main.c
@@ -0,0 +1,16 @@
+/*
+ * Projekt.c
+ *
+ * Created: 2017. 05. 16. 17:10:55
+ * Author : kazy
+ */
+
+#include <avr/io.h>
+
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
+
